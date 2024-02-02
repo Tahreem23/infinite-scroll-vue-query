09d889b15fe4e66b672c8bd535fecbcfb830e1b2
Tahreem Iqbal <tahreem.iqbal23@gmail.com>
2024-02-02 17:04:27 +0400

    Add files via upload

---
+++ InfiniteScroll.vue
@@ -0,0 +1,62 @@
+<template>
+  <span v-if="isLoading">Loading...</span>
+  <span v-else-if="isError">Error: {{ error.message }}</span>
+  <div v-else>
+    <span v-if="isFetching && !isFetchingNextPage">Fetching...</span>
+    <ul v-for="(group, index) in data.pages" :key="index">
+      <li v-for="user in group.results" :key="user.login.uuid">
+        <img :src="user.picture.thumbnail" alt="user thumbnail" />
+        <p>{{ user.name.first }} {{ user.name.last }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+
+<script setup>
+import { defineComponent, ref, onMounted } from "vue";
+import { useInfiniteQuery } from "vue-query";
+
+const currentPage = ref(1); // Assuming currentPage is a reactive variable
+
+const fetchProjects = async ({ pageParam = 1 }) => {
+  const response = await fetch(`https://randomuser.me/api/?page=${pageParam}&results=20&seed=abc`);
+  if (!response.ok) {
+    throw new Error('Failed to fetch projects');
+  }
+  return response.json();
+};
+
+function useProjectsInfiniteQuery() {
+  return useInfiniteQuery("projects", fetchProjects, {
+    getNextPageParam: (lastPage) => lastPage.info.page + 1,
+  });
+}
+
+const {
+  data,
+  error,
+  fetchNextPage,
+  hasNextPage,
+  isFetching,
+  isFetchingNextPage,
+  isLoading,
+  isError,
+} = useProjectsInfiniteQuery();
+
+const handleScroll = () => {
+ // console.log('handleScroll', window.innerHeight + window.scrollY, document.body.offsetHeight, !isFetchingNextPage);
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight &&
+   // !isFetchingNextPage &&
+    hasNextPage
+  ) {
+    fetchNextPage.value();
+  }
+};
+
+onMounted(() => {
+  window.addEventListener("scroll", handleScroll);
+});
+
+</script>
